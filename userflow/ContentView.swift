1958d393efbda601c0f179b08f6c7fcb57944319
Miguel Tomairo <matomairo@gmail.com>
2026-05-07 17:50:29 -0500

    upload initial project

---
+++ userflow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  userflow
+//
+//  Created by miguel tomairo on 7/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
